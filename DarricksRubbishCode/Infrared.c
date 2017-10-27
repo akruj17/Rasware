33756fca635794b2e751fb67166409bc915b0a39
sushibend <cheeeyman78@gmail.com>
2017-10-27 17:31:51 -0500

    try this for infrared sensor

--- DarricksRubbishCode/Infrared.c
+++ DarricksRubbishCode/Infrared.c
@@ -1,13 +1,14 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
-
+#include <RASLib/inc/adc.h>
 
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
 
     tADC *dist = InitializeADC(PIN_xx);
+    // Replace PIN_xx with an actual pin
 float distval;
     while (1) {
       distval = ADCRead(dist);
