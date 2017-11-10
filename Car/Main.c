e930fc9f2f5b9a5762113e5a80664a6404e50831
Liyan06 <lytang06@gmai.com>
2017-11-10 16:56:15 -0600

    line sensor code added

--- Car/Main.c
+++ Car/Main.c
@@ -16,11 +16,6 @@ tMotor *rightMotor;
 float leftSpeed;
 float rightSpeed;
 
-void blink(void) {
-    SetPin(PIN_B1, blink_on);
-    blink_on = !blink_on;
-}
-
 void setLeftMoterSpeed(float speed){
   SetMotor(leftMotor, speed);
   leftSpeed = speed;
@@ -33,76 +28,112 @@ void setRightMoterSpeed(float speed){
 
 int main(void) {
 
-    // Initialization part
-
-    //CallEvery(blink, 0, 0.5);
-
-    //Switch
-    //InitSwitch();
-
     //Continuous Servo Motor; true and false make motors
     //turn into opposite directions.
-    leftMotor = InitializeServoMotor(PIN_E4, false); //forward with false
-    rightMotor = InitializeServoMotor(PIN_E5, true);  // forward
+    // leftMotor = InitializeServoMotor(PIN_F4, false); //forward with false
+    // rightMotor = InitializeServoMotor(PIN_D7, true);  // forward
 
-    // Micro Servo Motor
-    // tServo *servo = InitializeServo(PIN_XX);
     //
-    // LineSensor
-    // tLineSensor *line = InitializeGPIOLineSensor(PIN_B0, PIN_B1, PIN_E4, PIN_E5, PIN_B4, PIN_A5, PIN_A6, PIN_A7);
-    // float value[8];
+    //LineSensor
+     tLineSensor *line = InitializeGPIOLineSensor(PIN_B0, PIN_B1, PIN_E4, PIN_E5, PIN_B4, PIN_A5, PIN_A6, PIN_A7);
+     float value[8];
     //
     // Infrared Distance Sensor
     // ADC is only supported on a limited number of pins in hardware
     // The following pins are supported:
     // PIN_B4 PIN_B5 PIN_D0  PIN_D1   PIN_D2  PIN_D3  PIN_E0
     // PIN_E1  PIN_E2  PIN_E3  PIN_E4  PIN_E5
-    //tADC *IRLeft = InitializeADC(PIN_D1);
-    float readIRLeft;
-    float error;
-    float percentError;
-    //float allError = 0;
+    float errorL;
+    float errorR;
+    float percentErrorL;
+    float percentErrorR;
     float desired = 0.40;
-    float kp;
-    //float ki;
+    float kpL;
+    float kpR;
+    tADC *IRLeft = InitializeADC(PIN_D1);
     tADC *IRRight = InitializeADC(PIN_D0);
+    float readIRLeft;
     float readIRRight;
-    float output;
-
-
+    int flag = 0;
 
     while (1) {
         // //Continuous Servo Motor V(left, 0) = V(right0.258)
         // // speed = changing k * speed(set)
-        setLeftMoterSpeed(0.8);  //< -0.3: backward
-        setRightMoterSpeed(0.8); // 0; 0.3
+        // setLeftMoterSpeed(0.8);  //< -0.3: backward
+        // setRightMoterSpeed(0.8); // 0; 0.3
         //
         //
-        // IRSensor
         // readIRLeft = ADCRead(IRLeft);
-        // Printf("IR Sensor1 value is %f\n", readIRLeft);
-        readIRRight = ADCRead(IRRight);
-        error = readIRRight - desired;
-        // percentError from -1 to 4; when error = 0, percentError = 0
-        percentError = error / desired - 0.5;
-        // percent Error from -4 to 4; when error = 0, percentError = 0
-        kp = 1 + fabs(0.4 * percentError);
-        //output = error * kp;
-        if (error > 0.1){
-          setLeftMoterSpeed(-0.2 * kp); // -0.4
-          setRightMoterSpeed(0.5 * kp);
-        }
-        else if (error < -0.1){
-          setLeftMoterSpeed(0.5 * kp);
-          setRightMoterSpeed(-0.2 * kp); //0.2
-        }
-        // if (error < 0){
-        //   Printf("The error is %f,    too far.\n", error);
+        // errorL = readIRLeft - desired;
+        // percentErrorL = errorL / desired - 0.5;
+        // kpL = 1 + fabs(0.4 * percentErrorL);
+
+        // readIRRight = ADCRead(IRRight);
+        // errorR = readIRRight - desired;
+        // percentErrorR = errorR / desired - 0.5;
+        // kpR = 1 + fabs(0.4 * percentErrorR);
+
+        // // Which IR sensor to use?
+        // if ((errorL - errorR < 0.1 || errorR - errorL < 0.1) && (errorL < 0.1 && errorR < 0.1)){
+        //   setLeftMoterSpeed(0.8);  //< -0.3: backward
+        //   setRightMoterSpeed(0.8);
         // }
-        // else {
-        //   Printf("The error is %f,    too close.\n", error);
+        //
+        // //if L - R > 0.05 : use L
+        // if ((errorL - errorR < 0.1 || errorR - errorL < 0.1) && (errorL > 0.1 || errorR > 0.1)){
+        //   //turn Left
+        //   int flag = 0;
+        //   while (flag < 6 ){
+        //     setLeftMoterSpeed(- 0.2 * kpR);
+        //     setRightMoterSpeed(0.5 * kpR);
+        //     Wait(0.2);
+        //     flag++;
+        //   }
         // }
-        Wait(0.01);
+        //
+        // //if R - L > 0.05 : use R
+        //
+        //
+        // //Left IR Sensor
+        // if (errorL > 0.1){
+        //   setLeftMoterSpeed(0.5 * kpL); // -0.4
+        //   setRightMoterSpeed(-0.2 * kpL);
+        // }
+        // else if (errorL < - 0.1){
+        //   if (errorL < - 0.3){
+        //     int flag = 0;
+        //     while (flag < 6 ){
+        //       setLeftMoterSpeed(0.8);
+        //       setRightMoterSpeed(0.8);
+        //       Wait(0.2);
+        //       flag++;
+        //     }
+        //   }
+        //   setLeftMoterSpeed(-0.2 * kpL);
+        //   setRightMoterSpeed(0.5 * kpL); //0.2
+        // }
+
+        //Right IR sensor
+        // if (errorR > 0.1){
+        //   setLeftMoterSpeed(-0.2 * kpR); // -0.4
+        //   setRightMoterSpeed(0.5 * kpR);
+        // }
+        // else if (errorR < - 0.1){
+        //   if (errorR < - 0.3){
+        //     flag = 1;
+        //     while (flag){
+        //       setLeftMoterSpeed(0.8);
+        //       setRightMoterSpeed(0.8);
+        //       Wait(1.5);
+        //       setLeftMoterSpeed(0.5 * kpR);
+        //       setRightMoterSpeed(-0.2 * kpR);
+        //       Wait(0.7);  //
+        //       flag = 0;
+        //     }
+        //   }
+        //   setLeftMoterSpeed(0.5 * kpR);
+        //   setRightMoterSpeed(-0.2 * kpR); //0.2
+        // }
 
         // Micro Servo Motor
         // float a;
@@ -116,8 +147,58 @@ int main(void) {
         // }
 
         //LineSensor
-        //LineSensorReadArray(line, value);
-        //Printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t\n", value[0], value[1], value[2],
-  //value[3], value[4], value[5], value[6], value[7]);
+        int max1 = 0;
+        int max2 = 0;
+        int index1, index2, sum;
+        LineSensorReadArray(line, value);
+        // find Max
+        // for (int i = 0; i <= 7; i++){
+        //   if (value[i] >= max1) {
+        //     max1 = value[i];
+        //     index1 = i;
+        //   }
+        // }
+        // // find second Max
+        // for (int i = 0; i <= 7; i++){
+        //   if (value[i] >= max2 && value[i] < max1){
+        //     max2 = value[i];
+        //     index2 = i;
+        //   }
+        // }
+        // for (int i = 0; i <= 7; i++){
+        //   if (i == index1 || i == index2){
+        //     value[i] = 1;
+        //   }
+        //   else{
+        //     value[i] = 0;
+        //   }
+        // }
+        // // reset value
+        //
+        int weight[8] = {-4,-3,-2,-1,1,2,3,4};
+        int outputArr[8];
+        for (int i=0; i<8; i++){
+          if (value[i] == INFINITY){
+            value[i] = 1;
+          }
+          else{
+            value[i] = 0;
+          }
+        }
+        for(int i = 0; i < 8; i++){
+          outputArr[i] = weight[i]*value[i];
+        }
+        for (int i = 0; i <= 7; i++){
+          sum += outputArr[i];
+        }
+        // setLeftMoterSpeed(0.8 + 0.04 * sum);
+        // setRightMoterSpeed(0.8 - 0.04 * sum);
+
+        //Printf("%d\n", sum);
+        Printf("%d\n", sum);
+  //       Printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t\n", value[0], value[1], value[2],
+  // value[3], value[4], value[5], value[6], value[7]);
+        //Wait(0.01);
+        Wait(0.5);
     }
 }
